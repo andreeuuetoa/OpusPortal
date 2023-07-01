31d178faad58a443b92ea2b43ef2db3b24a66c05
Andree Uuetoa <andree.uuetoa@gmail.com>
2023-07-01 09:44:33 +0300

    Unnecessary duplicate person ID

--- Public.DTO/v1.0/Identity/Person.cs
+++ Public.DTO/v1.0/Identity/Person.cs
@@ -4,7 +4,6 @@ namespace Public.DTO.v1._0.Identity;
 
 public class Person : DTOEntityId
 {
-    public Guid PersonId { get; set; }
     public string FirstName { get; set; } = default!;
     public string LastName { get; set; } = default!;
 }
